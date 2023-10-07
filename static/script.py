b697953067c2e14cb13bda6cc2b669df5ad8053b
Chirag20047 <chirag20047@iiitd.ac.in>
2023-10-07 14:13:42 +0000

    Minor changes

--- static/script.py
+++ static/script.py
@@ -2,25 +2,15 @@ from PIL import Image
 
 def shrink_image(input_path, output_path, max_size=(40, 40), quality=85):
     try:
-        # Open the image
         img = Image.open(input_path)
-        # print(img.size)
-        # Resize the image while maintaining its aspect ratio
         img.thumbnail(max_size)
-        
-        # Save the resized image with the specified quality
         img.save(output_path, optimize=True, quality=quality)
-        
         return True
     except Exception as e:
-        print(f"Error: {e}")
         return False
 
 if __name__ == "__main__":
-    input_path = "edit_icon5.png"  # Replace with the path to your uploaded image
-    output_path = "edit_icon5.png"  # Replace with the desired output path
-    
+    input_path = "edit_icon5.png"  
+    output_path = "edit_icon5.png"  
     if shrink_image(input_path, output_path):
-        print("Image successfully resized and compressed.")
-    else:
-        print("Failed to resize and compress the image.")
+        print("SUCCESS")
