bc01e5dc00daef6929287faad2f68bbd592ee28f
Chirag20047 <chirag20047@iiitd.ac.in>
2023-10-14 20:00:35 +0000

    Added accepted application page

--- static/script.py
+++ static/script.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-def shrink_image(input_path, output_path, max_size=(95, 95), quality=85):
+def shrink_image(input_path, output_path, max_size=(120, 120), quality=85):
     try:
         img = Image.open(input_path)
         img.thumbnail(max_size)
@@ -10,8 +10,8 @@ def shrink_image(input_path, output_path, max_size=(95, 95), quality=85):
         return False
 
 if __name__ == "__main__":
-    input_path = "accept_icon.jpg"  
-    output_path = "accept_icon.jpg"  
+    input_path = "report_user.jpg"  
+    output_path = "report_user.jpg"  
     if shrink_image(input_path, output_path):
         print("SUCCESS")
     else:
