9555955978ab0408571bac7254debdc1179b467e
Chirag20047 <chirag20047@iiitd.ac.in>
2023-10-21 17:40:23 +0000

    Public-Private Keys are getting generated and are in downloadable format

--- project/settings.py
+++ project/settings.py
@@ -119,13 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 # Collected files will be stored at the following location.
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # print(BASE_DIR)
 # print(STATIC_ROOT)
 # print(STATIC_URL)
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [
+    BASE_DIR,"static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
