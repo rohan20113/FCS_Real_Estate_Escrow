c40bf00d70aaacf236cd58e59588f9f7a3c0c71c
Chirag20047 <chirag20047@iiitd.ac.in>
2023-10-21 16:22:08 +0000

    Restarting the static objects work from here

--- project/settings.py
+++ project/settings.py
@@ -121,10 +121,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # Collected files will be stored at the following location.
-STATIC_ROOT = os.path.join(BASE_DIR, '../static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+print(BASE_DIR)
 # print(STATIC_ROOT)
 # print(STATIC_URL)
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
