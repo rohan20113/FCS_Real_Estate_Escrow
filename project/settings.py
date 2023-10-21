bae523a1e5f1d20a7ab1efe4b30d0ddefa0a2c3f
Chirag20047 <chirag20047@iiitd.ac.in>
2023-10-21 15:33:54 +0000

    Testing static objects & directories

--- project/settings.py
+++ project/settings.py
@@ -119,9 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/home/iiitd/dj_dir/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = '/home/iiitd/dj_dir/project/static/'
+# Collected files will be stored at the following location.
+STATIC_ROOT = os.path.join(BASE_DIR, 'project/static/')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
