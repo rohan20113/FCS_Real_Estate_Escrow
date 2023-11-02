acda018d4163a3a58035eae9300328e5c2c7400b
Chirag20047 <chirag20047@iiitd.ac.in>
2023-11-02 18:44:15 +0000

    Added download contract feature

--- static/script.py
+++ static/script.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-def shrink_image(input_path, output_path, max_size=(60, 60), quality=100):
+def shrink_image(input_path, output_path, max_size=(50 , 50), quality=100):
     try:
         img = Image.open(input_path)
         img.thumbnail(max_size)
@@ -10,8 +10,8 @@ def shrink_image(input_path, output_path, max_size=(60, 60), quality=100):
         return False
 
 if __name__ == "__main__":
-    input_path = "view_contract.png"  
-    output_path = "view_contract.png"  
+    input_path = "download_icon.png"  
+    output_path = "download_icon.png"  
     if shrink_image(input_path, output_path):
         print("SUCCESS")
     else:
