af54c99ce818fc126e312947933a6c282125279f
Chirag20047 <chirag20047@iiitd.ac.in>
2023-11-02 16:04:54 +0000

    Added view_contract image and modified css style classes and Ruppees symbol

--- static/script.py
+++ static/script.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-def shrink_image(input_path, output_path, max_size=(120, 120), quality=85):
+def shrink_image(input_path, output_path, max_size=(60, 60), quality=100):
     try:
         img = Image.open(input_path)
         img.thumbnail(max_size)
@@ -10,8 +10,8 @@ def shrink_image(input_path, output_path, max_size=(120, 120), quality=85):
         return False
 
 if __name__ == "__main__":
-    input_path = "report_user.jpg"  
-    output_path = "report_user.jpg"  
+    input_path = "view_contract.png"  
+    output_path = "view_contract.png"  
     if shrink_image(input_path, output_path):
         print("SUCCESS")
     else:
